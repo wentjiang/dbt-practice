f8ce7eb035ad9a58de409bd413b39108a11a7146
wentjiang <wentjiang@sina.com>
2026-06-08 22:20:38 +0800

    feat: dbt vault link models for transaction-property, buyer, seller, agent

---
+++ dbt/models/vault/links/lnk_transaction_agent.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='transaction_hk',
+    schema='gold'
+  )
+}}
+
+with source as (
+    select
+        transaction_hk,
+        agent_hk,
+        load_date,
+        record_source
+    from {{ ref('stg_transactions') }}
+)
+
+select * from source
+
+{% if is_incremental() %}
+where transaction_hk not in (select transaction_hk from {{ this }})
+{% endif %}
